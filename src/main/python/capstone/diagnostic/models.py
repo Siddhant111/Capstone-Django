251da4e575b49017b95f329e9e74e74733bd1869
Ajinkya Bedekar <ajinkyabedekar2@gmail.com>
2019-11-14 12:30:28 +0530

    Tensorflow Models

--- src/main/python/capstone/diagnostic/models.py
+++ src/main/python/capstone/diagnostic/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Document(models.Model):
+    document = models.ImageField(upload_to='documents/%Y/%m/%d/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
--- src/main/python/capstone/diagnostic/views.py
+++ src/main/python/capstone/diagnostic/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+import os
+
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 
 # Create your views here.
 
 def diagnostic(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        file_path = os.path.join(settings.BASE_DIR, 'models\\research\\object_detection')
+        os.system('python ' + file_path + '\\object_detection_tutorial.py')
     return render(request, 'diagnostic.html')
