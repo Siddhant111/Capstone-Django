ec68885bbd4edca0d2f2929736d4eb584c9f26e3
Ajinkya Bedekar <ajinkyabedekar2@gmail.com>
2019-11-14 13:32:49 +0530

    Diagnostic Initialization

--- src/main/python/capstone/diagnostic/views.py
+++ src/main/python/capstone/diagnostic/views.py
@@ -7,11 +7,7 @@ from django.conf import settings
 # Create your views here.
 
 def diagnostic(request):
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        uploaded_file_url = fs.url(filename)
+    if request.method == 'POST':
         file_path = os.path.join(settings.BASE_DIR, 'models\\research\\object_detection')
         os.system('python ' + file_path + '\\object_detection_tutorial.py')
     return render(request, 'diagnostic.html')
